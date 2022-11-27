016de50132a772a1348050e6f1e090d155faa55b
s95528 <s95528@pollub.edu.pl>
2022-11-27 13:18:18 +0100

    Adding nLogger

--- Program.cs
+++ Program.cs
@@ -2,39 +2,67 @@ using System.Reflection;
 using DotnetApp;
 using DotnetApp.Entities;
 using DotnetApp.Services;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<RestaurantDbContext>();
-builder.Services.AddScoped<Resturantseeder>();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<RestaurantDbContext>();
+    builder.Services.AddScoped<Resturantseeder>();
+    builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+    builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+    builder.Services.AddControllersWithViews();
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<Resturantseeder>();
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<Resturantseeder>();
 
 // Configure the HTTP request pipeline.
-seeder.Seed();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    seeder.Seed();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
--- Services/RestaurantService.cs
+++ Services/RestaurantService.cs
@@ -19,11 +19,13 @@ public class RestaurantService : IRestaurantService
 {
     private readonly RestaurantDbContext _dbContext;
     private readonly IMapper _mapper;
+    private readonly ILogger<RestaurantService> _logger;
 
-    public RestaurantService(RestaurantDbContext dbContext, IMapper mapper)
+    public RestaurantService(RestaurantDbContext dbContext, IMapper mapper, ILogger<RestaurantService> logger)
     {
         _dbContext = dbContext;
         _mapper = mapper;
+        _logger = logger;
     }
     
     public RestauantDTO GetById(int id)
@@ -58,6 +60,7 @@ public class RestaurantService : IRestaurantService
 
     public int Create(CreateRestaurantDto dto)
     {
+        _logger.LogWarning($"Restaurant CREATE action invoked");
         var restaurant = _mapper.Map<Restauant>(dto);
         _dbContext.Restauants.Add(restaurant);
         _dbContext.SaveChanges();
@@ -87,6 +90,8 @@ public class RestaurantService : IRestaurantService
     
     public bool Delete(int id)
     {
+        _logger.LogWarning($"Restaurant with id: {id} DELETE action invoked");
+        
         var restaurant = _dbContext.Restauants
             .FirstOrDefault(r => r.Id == id);
         
